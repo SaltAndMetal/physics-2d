ca61c87996c4492bbcbc3b2ffe977598ac1685e6
Max Hamilton <max.alexander.hamilton@gmail.com>
2022-02-11 12:12:04 +1100

    Revert "Broken. commmitting so I can revert and see how it was before"
    This reverts commit f986308bec8106a3b6b9c0038f98189b3ae2b1de.

--- src/displayable/physics/rect.rs
+++ src/displayable/physics/rect.rs
@@ -21,13 +21,23 @@ impl Displayable for Rect
 {
     fn display(&self) -> Vec<(Point, Color)>
     {
+        let pointA = self.bottomLeft();
+
+        let pointB = pointA + Vec2::new(self.rotation.sin()*self.topRight().x(), self.rotation.cos()*self.topRight().y());
+
+        let secondAngle = self.rotation()+std::f64::consts::FRAC_PI_2;
+        let pointC = pointB + Vec2::new(secondAngle.sin()*self.topRight().x(), secondAngle.cos()*self.topRight().y());
+
+        let thirdAngle = self.rotation()+std::f64::consts::PI;
+        let pointD = pointC + Vec2::new(thirdAngle.sin()*self.topRight().x(), thirdAngle.cos()*self.topRight().y());
+
         let extr = |a: Vec2| {let a = Point::from(a); (a.x() as isize, a.y() as isize)};
         let revExtr = |a: (isize, isize)| {(Point::new(a.0 as i32, a.1 as i32), self.color())};
 
-        let points: Vec<(Point, Color)> = Bresenham::new(extr(self.points[0]), extr(self.points[1]))
-            .chain(Bresenham::new(extr(self.points[1]), extr(self.points[2])))
-            .chain(Bresenham::new(extr(self.points[2]), extr(self.points[3])))
-            .chain(Bresenham::new(extr(self.points[3]), extr(self.points[0])))
+        let points: Vec<(Point, Color)> = Bresenham::new(extr(pointA), extr(pointB))
+            .chain(Bresenham::new(extr(pointB), extr(pointC)))
+            .chain(Bresenham::new(extr(pointC), extr(pointD)))
+            .chain(Bresenham::new(extr(pointD), extr(pointA)))
             .map(|a| revExtr(a))
             .collect();
             
